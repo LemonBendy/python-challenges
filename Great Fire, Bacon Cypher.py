dd0a6e63ff50f954770b8e41ffe6049e683deb10
LemonBendy <ben.deaville@gmail.com>
2022-09-22 09:48:46 +0100

    Added new programs

---
+++ Great Fire, Bacon Cypher.py
@@ -0,0 +1,6 @@
+
+
+msg = "the great fire was an accident oN ThE SEconD Of SEPTEmber, The KiNGs BAkER LeFT ThE EMbeRS oF HIs ovEN sMOuLDeRiNG THIS SET LiGHT tO SoME fiRewOOD Left nearby three hours later his bakery in pudding lane was on fi the winds then preade the fire across london"
+rmmsg = msg.replace(" ", "")
+x = int(len(rmmsg))
+print(rmmsg)
