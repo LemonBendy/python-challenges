94d46be66515ae8cf3a3be2c7ea6b14aaa0784e0
Ben Deaville (A29762) <A29762@sfc.potteries.ac.uk>
2022-09-23 14:10:31 +0100

    attempting to finalise

--- fruit toller.py
+++ fruit toller.py
@@ -1,7 +1,11 @@
 from random import randint
+import time
 
 playing = True
 money = 100
+
+def credit_calc(money):
+    return f"£{round(abs(money)/100, 2):.2f}"
 
 def roll(roll_number):
   match roll_number:
@@ -18,16 +22,62 @@ def roll(roll_number):
     case 5:
       return "skull"
 
+def double_reward(money):
+    money = money + 50
+    return money
+
+def triple_reward(money):
+    money = money + 100
+    return money
+
+def bell_bonus(money):
+    money = money + 500
+    return money
+
+def bankrupt(money):
+    money = 0
+    return money
+
+def db_skull(money):
+    money = money - 100
+    return money
 
 while playing == True:
     player = input("would you like to play: Y/N")
     if player == 'Y' or 'y':
-        print(money + " -20")
+        print ("-20p")
+        print(". . . rolling . . .")
+        time.sleep(1)
         money = money - 20
-        roll1 = str(roll(randint(0,5)))
-        roll2 = str(roll(randint(0,5)))
-        roll3 = str(roll(randint(0,5)))
-        print(roll1 + ' : ' + roll2 + ' : ' + roll3)
-        if roll1 and roll2 == "star" or roll2 and roll3 == "star" or roll1 and roll3 == "star":
-            print("whoop")
+        r1 = str(roll(randint(0,5)))
+        r2 = str(roll(randint(0,5)))
+        r3 = str(roll(randint(0,5)))
+        print(r1 + ' : ' + r2 + ' : ' + r3)
+        if r1 == "skull" and r2 == "skull" and r3 == "skull":
+            bankrupt(money)
+            time.sleep(0.4)
+            print(credit_calc(money))
+        elif r1 == "skull" and r2 == r1 or r2 == "skull" and r3 == r2 or r1 == "skull" and r3 == r1:
+            db_skull(money)
+            time.sleep(0.4)
+            print(credit_calc(money))
+        elif r1 == "bell" and r1 == r2 and r1 == r3:
+            bell_bonus(money)
+            time.sleep(0.4)
+            print("whoop, whoop")
+            print(credit_calc(money))
+        elif r1 == r2 and r1 == r3:
+            triple_reward(money)
+            time.sleep(0.4)
+            print("triple riple")
+            print(credit_calc(money))
+        elif r1 == r2 or r2 == r3 or r1 == r3:
+            double_reward(money)
+            time.sleep(0.4)
+            print("double trouble")
+            print(credit_calc(money))
+        else:
+            time.sleep(0.4)
+            print("unlucky, better luck next time")
+            print(credit_calc(money))
 
