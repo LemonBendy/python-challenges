c594419b498f14e2b3ea4615fd2dff81053f59da
LemonBendy <88690369+LemonBendy@users.noreply.github.com>
2022-10-01 21:08:37 +0100

    Happy Number finalised

--- Happy Numbers.py
+++ Happy Numbers.py
@@ -1,5 +1,8 @@
 
 ## The First 8 numbers are 1, 7, 10, 13, 19, 23, 28, 31
+# read around the topic a little, seems to be quite interesting. It seems to be that you could...
+# optimise it after the first value if it is equal to certain values <600. Maybe something for a later date
+
 
 def square_num(n):
   result = 0
@@ -19,13 +22,13 @@ def Happy_Number(number):
       numbers.append(number)
   return happy
 
-happy = []
+happy_list = []
 n = 1
 count = 0
 while count < 8:
   if Happy_Number(n):
     count +=1
-    happy.append(n)
+    happy_list.append(n)
   n += 1
 print("The first eight happy numbers are:")
-print(happy)
+print(happy_list)
