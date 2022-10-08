24b6ee99e5604245d16c781db9c747645f36bd5f
LemonBendy <88690369+LemonBendy@users.noreply.github.com>
2022-10-08 22:12:50 +0100

    Updated Arithmetic Test 10

--- Arithmetic Test Challenge 10.py
+++ Arithmetic Test Challenge 10.py
@@ -1,16 +1,19 @@
-import random
+import secrets
 from random import randint
 
 
-#name = input("Name:")
+name = input("Name:")
+
+
 def random_int(x):
-    n1 = randint(0,x)
-    n2 = randint(0,x)
+    n1 = randint(0, x)
+    n2 = randint(0, x)
     return n1, n2
+
 
 def multiple():
     n1, n2 = random_int(12)
-    question = int(input("What is " + str(n1) + " x " + str(n2)))
+    question = int(input("What is " + str(n1) + " x " + str(n2) + ": "))
     correct = n1*n2
     if question == correct:
         print("True")
@@ -19,8 +22,8 @@ def multiple():
 
 
 def addition():
-    n1,n2 = random_int(99)
-    question = int(input("What is " + str(n1) + " + " + str(n2)))
+    n1, n2 = random_int(99)
+    question = int(input("What is " + str(n1) + " + " + str(n2) + ": "))
     correct = n1+n2
     if question == correct:
         print("True")
@@ -29,8 +32,8 @@ def addition():
 
 
 def subtraction():
-    n1,n2 = random_int(99)
-    question = int(input("What is " + str(n1) + " - " + str(n2)))
+    n1, n2 = random_int(99)
+    question = int(input("What is " + str(n1) + " - " + str(n2) + ": "))
     correct = n1-n2
     if question == correct:
         print("True")
@@ -39,15 +42,18 @@ def subtraction():
 
 
 def division():
-    n1 = randint(0,99)
-    n2 = random.sample(1,2,4,5,8,10)
-    question = int(input("What is " + str(n1) + " " + str(n2)))
+    n1 = randint(0, 99)
+    nums = (1, 2, 4, 5, 8, 10)
+    n2: int = secrets.choice(nums)
+    question = float(input("What is " + str(n1) + " ÷ " + str(n2) + ": "))
     correct = n1/n2
     if question == correct:
         print("True")
     else:
         print("False")
 
+
 addition()
 subtraction()
 division()
+multiple()
