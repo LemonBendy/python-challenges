933ecdd848c6e9363cbfdb739764e177a899cf67
LemonBendy <88690369+LemonBendy@users.noreply.github.com>
2022-10-08 21:53:18 +0100

    Added Binary to Denary calculator
    Added in extra template functions for later development (in comments)

--- Binary Calculator.py
+++ Binary Calculator.py
@@ -1,18 +1,44 @@
 
 
+# def binary_to_hex(): for later use
+
+
+# def hex_to_binary(): for later use
+
+
+# def denary_to_hex(): for later use
+
+
+# def hex_to_denary(): for later use
+
+
 def denary_to_binary():
-    num = int(input(("Input your denary number:")))
+    num = int(input("Input your denary number:"))
     binary = str("")
 
     while num != 0:
         n1 = num//2
-        n2 = num%2
+        n2 = num % 2
         binary = str(n2) + binary
         num = n1
     return binary
 
 
 def binary_to_denary():
-    binary = str(input("Input Binary:"))
-    pmax = int(len(binary) - 1)
+    num = int(input("Input Binary:"))
+    denary = 0
+    power = 1
+    while num > 0:
+        rem = num % 10
+        num = num//10
+        denary += rem*power
+        power = power*2
+    return denary
 
+    
+select = input("Would you like to convert (B)inary or (D)enary: ")
+
+if select.lower() == 'd':
+    print(denary_to_binary())
+elif select.lower() == 'b':
+    print(binary_to_denary())
