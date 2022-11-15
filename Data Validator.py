2caf10d71a206893d64cafab905abff3735b80f5
LemonBendy <88690369+LemonBendy@users.noreply.github.com>
2022-11-15 10:16:11 +0000

    Optimisation

--- Data Validator.py
+++ Data Validator.py
@@ -9,50 +9,27 @@
 
 def isleap(year: int):
     leap_result: int = int(year) % 4
-    if int(leap_result) == 0:
-        return True
-    else:
-        return False
+    return int(leap_result) == 0
 
 
 def isvalid(date):
     if len(date) > 6:
         day = int(date[0:2])
-        month = int(date[3:5])
-        year = int(date[6:])
+        month = date[3:5]
+        year = date[6:]
         if 12 >= int(month) > 0:
             print("correct month")  # remove once done
             month_new = str(date[3:5])
             match str(month_new):
-                case "01", "03", "05", "07", "08", "10", "12":
-                    if 31 >= day > 0:
-                        print("31 true")
-                        return True
-                    else:
-                        print("31 false")
-                        return False
-                case "04", "06", "09", "11":
-                    if 30 >= day > 0:
-                        print("30 true")
-                        return True
-                    else:
-                        print("30 false")
-                        return False
+                case "01" | "03" | "05" | "07" | "08" | "10" | "12":
+                    return 31 >= day > 0
+                case "04" | "06" | "09" | "11":
+                    return 30 >= day > 0
                 case "02":
                     if isleap(year):
-                        if 29 >= day > 0:
-                            print("leaped")
-                            return True
-                        else:
-                            print("leaped false")
-                            return False
+                        return 29 >= day > 0
                     else:
-                        if 28 >= day > 0:
-                            print("not leaped")
-                            return True
-                        else:
-                            print("not leaped false")
-                            return False
+                        return 28 >= day > 0
         else:
             print("Incorrect Date, format")
             return False
