06cbd5e526373b7fc469859b8149bf4703d77648
LemonBendy <88690369+LemonBendy@users.noreply.github.com>
2022-12-02 13:32:16 +0000

    Sample Space, connect 4

--- Connect 4.py
+++ Connect 4.py
@@ -1,4 +1,5 @@
-from random import choice
+import random
+import sys
 
 board = [["0", "0", "0", "0", "0"],
          ["0", "0", "0", "0", "0"],
@@ -11,35 +12,71 @@ board = [["0", "0", "0", "0", "0"],
 def show_board():
     global board
     for row in range(0, 6):
-        for column in range (0,5):
-            print(board[row][column],"   ", end=' ')
+        for column in range(0, 5):
+            print(board[row][column], "", end=' ')
         print()
 
 
 def update_board(player):
+    global board
     column = input("Which Column: ")
     match column.lower():
         case "a":
-            print("A")
+            for i in range(0, 5):
+                if board[4-i][0] == "0":
+                    board[4-i][0] = player
+                    positionx = 0
+                    positiony = 4-i
+                    return positionx, positiony
         case "b":
-            print("B")
+            for i in range(0, 5):
+                if board[4 - i][1] == "0":
+                    board[4 - i][1] = player
+                    positionx = 1
+                    positiony = 4 - i
+                    return positionx, positiony
         case "c":
-            print("C")
+            for i in range(0, 5):
+                if board[4 - i][2] == "0":
+                    board[4 - i][2] = player
+                    positionx = 2
+                    positiony = 4 - i
+                    return positionx, positiony
         case "d":
-            print("D")
+            for i in range(0, 5):
+                if board[4 - i][3] == "0":
+                    board[4 - i][3] = player
+                    positionx = 3
+                    positiony = 4 - i
+                    return positionx, positiony
         case "e":
-            print("E")
+            for i in range(0, 5):
+                if board[4 - i][4] == "0":
+                    board[4 - i][4] = player
+                    positionx = 4
+                    positiony = 4 - i
+                    return positionx, positiony
+        case _:
+            print(f"'{column}' is not a column")
 
 
-def check_winner():
-    print()
-    #checks horizontal and vertical, diagonal at a later date
+def check_winner(x, y, player):
+    def horizontal(x):
+        if
+            return True
+
+            return False
+        # checks horizontal and vertical, diagonal at a later date
 
 
 def main():
-    show_board()
-    update_board()
-    #main game 
+    characters = ['x', 'y']
+    character = random.choice(characters)
+    while True:
+        show_board()
+        posx, posy = update_board(character)[0], update_board(character)[1]
+        if check_winner(posx, posy):
+            sys.exit()
 
 
-show_board()
+main()
