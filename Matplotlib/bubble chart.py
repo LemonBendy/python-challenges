a5ab97ba9da5231add0a25c9fd4ae63f7b1e335e
LemonBendy <88690369+LemonBendy@users.noreply.github.com>
2023-02-09 09:56:47 +0000

    hello

---
+++ Matplotlib/bubble chart.py
@@ -0,0 +1,6 @@
+import plotly.express as px
+df = px.data.gapminder()
+
+fig = px.scatter(df.query("year==2007"), x="gdpPercap", y="lifeExp",
+                 size="pop", colour="continent",
+                 hover_name="country", log_x=True, size_max=60)
