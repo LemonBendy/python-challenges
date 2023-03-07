5cd166939b17021033323356f02c5e4287383ea0
LemonBendy <88690369+LemonBendy@users.noreply.github.com>
2023-03-07 10:32:13 +0000

    Efficient Bubble Sort

--- Sort/Bubble Sort.py
+++ Sort/Bubble Sort.py
@@ -1,16 +1,36 @@
+import random
+import time
+
+def randomList(a):
+    list = []
+    for i in range(0, a):
+        list.append(random.randint(1, 100))
+    return list
+
+
 def bubbleSort(list):
+    time1 = time.perf_counter()
     for passnum in range(len(list)-1,0,-1):
         for i in range(passnum):
-            print(1)
             if list[i]>list[i+1]:
                 temp = list[i]
                 list[i] = list[i+1]
                 list[i+1] = temp
-                print(list)
+    time2 = time.perf_counter()
+    print(f"Time taken: {time2-time1}")
     return list
 
-abd = [23,47,15,44,95,11,30]
-list = [54,26,93,17,77,31,44,55,20]
-print(bubbleSort(abd))
+
+def main():
+    num = int(input("How many numbers do you want to sort: "))
+    list = randomList(num)
+    bubbleSort(list)
+
+
+if __name__ == "__main__":
+    while True:
+        main()
+        if input("Do you want to sort another list? (y/n): ") == "n":
+            break
 
 
