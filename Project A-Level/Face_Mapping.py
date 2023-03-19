cc7c7e31fdd7d0f27bf3bf6de4ba6c52f2308239
LemonBendy <88690369+LemonBendy@users.noreply.github.com>
2023-03-19 20:14:18 +0000

    Initial File for A-Level Project

---
+++ Project A-Level/Face_Mapping.py
@@ -0,0 +1,45 @@
+#imports
+import mediapipe as mp
+import cv2
+
+# --------------------
+# Face Mesh Setup
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_face_mesh = mp.solutions.face_mesh
+
+#web cam setup
+cap = cv2.VideoCapture(0)
+
+
+with mp_face_mesh.FaceMesh(
+    max_num_faces = 1,
+    refine_landmarks = True,
+    min_detection_confidence = 0.5,
+    min_tracking_confidence = 0.5 ) as face_mesh:
+
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            print("Empty camera frame, ignoring process")#if the camera is not working 
+            continue
+        image.flags.writeable = False # Make the image read only
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # Convert the BGR image to RGB before processing.
+        results = face_mesh.process(image) # Process the image
+        image.flags.writeable = True # Make the image read only
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # C
+        if results.multi_face_landmarks: # If the results are not empty
+            for face_landmarks in results.multi_face_landmarks: # For each face
+                #border around face, eyes and mouth
+                mp_drawing.draw_landmarks(
+                    image = image,
+                    landmark_list = face_landmarks,
+                    connections = mp.face_mesh.FACE_CONNECTIONS,
+                    landmark_drawing_spec = None,
+                    connection_drawing_spec = mp_drawing_styles
+                    .get_default_face_mesh_contours_style())
+        cv2.imshow('Face Mesh', cv2.flip(image, 1))
+        if cv2.waitKey(5) & 0xFF == 27: # If the user presses the escape key break the loop
+            break
+cap.release()
