17289e7de69045af019c098be11f2e644454dbf1
LemonBendy <88690369+LemonBendy@users.noreply.github.com>
2023-03-20 12:21:49 +0000

    Added image scaling and meshing
    Co-Authored-By: PineappleFan <40807954+PineaFan@users.noreply.github.com>

--- Project A-Level/Face_Mapping.py
+++ Project A-Level/Face_Mapping.py
@@ -14,7 +14,7 @@ cap = cv2.VideoCapture(0)
 
 
 with mp_face_mesh.FaceMesh(
-    max_num_faces = 1,
+    max_num_faces = 2,
     refine_landmarks = True,
     min_detection_confidence = 0.5,
     min_tracking_confidence = 0.5 ) as face_mesh:
@@ -35,11 +35,29 @@ with mp_face_mesh.FaceMesh(
                 mp_drawing.draw_landmarks(
                     image = image,
                     landmark_list = face_landmarks,
-                    connections = mp.face_mesh.FACE_CONNECTIONS,
+                    connections = mp_face_mesh.FACEMESH_CONTOURS,
                     landmark_drawing_spec = None,
                     connection_drawing_spec = mp_drawing_styles
                     .get_default_face_mesh_contours_style())
-        cv2.imshow('Face Mesh', cv2.flip(image, 1))
+                mp_drawing.draw_landmarks(
+                    image = image,
+                    landmark_list = face_landmarks,
+                    connections = mp_face_mesh.FACEMESH_TESSELATION,
+                    landmark_drawing_spec = None,
+                    connection_drawing_spec = mp_drawing_styles
+                    .get_default_face_mesh_tesselation_style())
+                mp_drawing.draw_landmarks(
+                    image = image,
+                    landmark_list = face_landmarks,
+                    connections = mp_face_mesh.FACEMESH_IRISES,
+                    landmark_drawing_spec = None,
+                    connection_drawing_spec = mp_drawing_styles
+                    .get_default_face_mesh_iris_connections_style())
+        image = cv2.flip(image, 1) # Flip the image horizontally for a selfie-view display
+        monitor_height = 650
+        new_width = int(image.shape[1] * monitor_height / image.shape[0])
+        image = cv2.resize(image, (new_width, monitor_height))
+        cv2.imshow('Face Mesh', image)
         if cv2.waitKey(5) & 0xFF == 27: # If the user presses the escape key break the loop
             break
 cap.release()
