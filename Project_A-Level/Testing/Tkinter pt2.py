7bf37f91624149a0f0100fd2747f960c98adbe27
LemonBendy <88690369+LemonBendy@users.noreply.github.com>
2023-04-28 14:17:35 +0100

    Tkinter Fixing

--- Project_A-Level/Testing/Tkinter pt2.py
+++ Project_A-Level/Testing/Tkinter pt2.py
@@ -1,6 +1,9 @@
 import tkinter
 import cv2
 import PIL.Image, PIL.ImageTk
+import time
+import threading
+
 
 class App:
     def __init__(self, window, window_title, video_source=0):
@@ -19,23 +22,55 @@ class App:
 
         self.window.mainloop()
 
-
     def update(self):
         ret, frame = self.vid.get_frame()
-
         if ret:
             self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
             self.canvas.create_image(0, 0, image = self.photo, anchor = tkinter.NW)
-
         self.window.after(self.delay, self.update)
-class MyVideoCapture:
+
+
+class KeyTracker():
+    key = ''
+    last_press_time = 0
+    last_release_time = 0
+
+    def track(self, key):
+        self.key = key
+
+    def is_pressed(self):
+        return time.time() - self.last_press_time < .1
+
+    def report_key_press(self, event):
+        if event.keysym == self.key:
+            if not self.is_pressed():
+                print(f"press {event.keysym}")
+            self.last_press_time = time.time()
+
+    def report_key_release(self, event):
+        if event.keysym == self.key:
+            timer = threading.Timer(.1, self.report_key_release_callback, args=[event])
+            timer.start()
+
+    def report_key_release_callback(self, event):
+        if not self.is_pressed():
+            print(f"release {event.keysym}")
+        self.last_release_time = time.time()
+
+
+class MyVideoCapture(App):
     def __init__(self, video_source=0):
         self.vid = cv2.VideoCapture(video_source)
+        
         if not self.vid.isOpened():
             raise ValueError("Unable to open source", video_source)
-
         self.width = self.vid.get(cv2.CAP_PROP_FRAME_WIDTH)
         self.height = self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+        key_tracker = KeyTracker()
+        self.bind_all('<KeyPress>', key_tracker.report_key_press)
+        self.bind_all('<KeyRelease>', key_tracker.report_key_release)
+        key_tracker.track('space')
 
     def __del__(self):
         if self.vid.isOpened():
